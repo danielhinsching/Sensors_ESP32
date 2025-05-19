1426b8e6d335532b7a192938e0f8191ead0287e2
danielhinsching <danielsantoshinsching@gmail.com>
2025-05-19 14:09:31 -0300

    Chore: Configuração teste conexão ao ESP32 DEVKITV1

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,14 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(2000);
+  Serial.println("SETUP >>>>>>>>>>>>>>>>>>");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println("LOOP >>>>>>>>>>>>>>>>>>");
+  delay(2000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+
